3434f8e6de95e2e3a151723d227d3abbf362e1bd
Lucas Campanelli <lucas.campanelli@outlook.com>
2022-06-20 01:51:55 -0300

    Inicializando o projeto Spring Boot

---
+++ src/main/java/com/pagueibaratoapi/pagueibaratoapi/PagueibaratoapiApplication.java
@@ -0,0 +1,13 @@
+package com.pagueibaratoapi.pagueibaratoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagueibaratoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagueibaratoapiApplication.class, args);
+	}
+
+}
