417993010440860b0fd1c063e83dcb239df3ed9b
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2022-06-21 22:31:53 -0300

    Model de Mercado e tipo de Resultado

---
+++ src/main/java/com/pagueibaratoapi/models/Mercado.java
@@ -0,0 +1,127 @@
+package com.pagueibaratoapi.models;
+
+public class Mercado {
+    
+    private int id;
+    private String nome;
+    private int ramo;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private String cep;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getRamo() {
+        return ramo;
+    }
+
+    public void setRamo(int ramo) {
+        this.ramo = ramo;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Resultado create(){
+        try {
+            return new Resultado(true, "Sucesso");
+        } catch (Exception e) {
+            return new Resultado(false, e.getLocalizedMessage());
+        }
+    }
+
+    public Resultado read(){
+        try {
+            return new Resultado(true, "Sucesso");
+        } catch (Exception e) {
+            return new Resultado(false, e.getLocalizedMessage());
+        }
+    }
+
+    public Resultado update(){
+        try {
+            return new Resultado(true, "Sucesso");
+        } catch (Exception e) {
+            return new Resultado(false, e.getLocalizedMessage());
+        }
+    }
+
+    public Resultado delete(){
+        try {
+            return new Resultado(true, "Sucesso");
+        } catch (Exception e) {
+            return new Resultado(false, e.getLocalizedMessage());
+        }
+    }
+}
