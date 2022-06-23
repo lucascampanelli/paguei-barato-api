bb7e482890547c80e0ed9f693594083d19da6c4c
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2022-06-23 01:17:32 -0300

    Testando a API

---
+++ src/main/java/com/pagueibaratoapi/PagueibaratoapiApplication.java
@@ -0,0 +1,13 @@
+package com.pagueibaratoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class PagueibaratoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagueibaratoapiApplication.class, args);
+	}
+}
