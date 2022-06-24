1c3fe62c9c82ab6f6edb2e5ebe1a4edcd302f4c8
Lucas Campanelli <lucas.campanelli@outlook.com>
2022-06-24 01:37:16 -0300

    Construtor para repository e create no controller

--- src/main/java/com/pagueibaratoapi/controllers/ProdutoController.java
+++ src/main/java/com/pagueibaratoapi/controllers/ProdutoController.java
@@ -3,20 +3,25 @@ package com.pagueibaratoapi.controllers;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pagueibaratoapi.models.Produto;
+import com.pagueibaratoapi.repository.ProdutoRepository;
 
 @RestController
 @RequestMapping("/produto")
 public class ProdutoController {
     
-    // @GetMapping
-    // public List<Produto> listar(){
-    //     try {
-            
-    //     } catch (Exception e) {
-    //         //TODO: handle exception
-    //     }
-    // }
+    private final ProdutoRepository produtoRepository;
+
+    public ProdutoController(ProdutoRepository produtoRepository){
+        this.produtoRepository = produtoRepository;
+    }
+
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public Produto criar(@RequestBody Produto produto){
+        return produtoRepository.save(produto);
+    }
 }
