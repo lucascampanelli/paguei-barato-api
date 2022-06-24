a6148e3320113d3256d0f18fabe5fc643d6e07d9
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2022-06-24 00:23:54 -0300

    Controller e model de mercado

--- src/main/java/com/pagueibaratoapi/PagueibaratoapiApplication.java
+++ src/main/java/com/pagueibaratoapi/PagueibaratoapiApplication.java
@@ -2,12 +2,11 @@ package com.pagueibaratoapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-public class PagueibaratoapiApplication {
+public class PagueiBaratoApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PagueibaratoapiApplication.class, args);
+		SpringApplication.run(PagueiBaratoApiApplication.class, args);
 	}
 }
--- src/main/java/com/pagueibaratoapi/controllers/MercadoController.java
+++ src/main/java/com/pagueibaratoapi/controllers/MercadoController.java
@@ -1,40 +1,49 @@
 package com.pagueibaratoapi.controllers;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import com.pagueibaratoapi.models.Mercado;
 
 @RestController
 @RequestMapping("/mercado")
 public class MercadoController {
     
-    @PostMapping
-    public String create(){
-        return "milu";
-    }
-
-    @GetMapping
-    public String read(){
-        return "Milu muito fofo";
+    @PostMapping(consumes = "application/json", produces = "application/json")
+    public Mercado criar(@RequestBody Mercado mercado){
+        mercado.create();
+        return mercado;
     }
 
     @GetMapping("/{nome}")
-    public String read(@PathVariable Object nome){
-        return "O "+nome+" é muito fofo";
+    public String ler(@PathVariable Object nome){
+        return "Olá, "+nome+"!";
     }
 
-    @PatchMapping
-    public void update(){
+    @GetMapping
+    public String listar(){
+        return "Bem vindo ao mercado!";
+    }
+
+    @PatchMapping("/{id}")
+    public void editar(@PathVariable int id){
 
     }
 
-    @DeleteMapping
-    public void delete(){
+    @PutMapping
+    public void atualizar(){
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable int id){
 
     }
 }
