70664e56b05da606e26b55c7498ed02e5e94fdec
Lucas Campanelli <lucas.campanelli@outlook.com>
2022-06-26 21:28:15 -0300

    DELETE do usuário

--- src/main/java/com/pagueibaratoapi/controllers/UsuarioController.java
+++ src/main/java/com/pagueibaratoapi/controllers/UsuarioController.java
@@ -2,7 +2,9 @@ package com.pagueibaratoapi.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +31,24 @@ public class UsuarioController {
     @PostMapping(consumes = "application/json", produces = "application/json")
     public Usuario criar(@RequestBody Usuario usuario){
         return usuarioRepository.save(usuario);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable int id){
+        Usuario usuarioDeletado = new Usuario();
+
+        usuarioDeletado.setId(id);
+        usuarioDeletado.setNome("");
+        usuarioDeletado.setEmail("");
+        usuarioDeletado.setSenha("");
+        usuarioDeletado.setLogradouro("");
+        usuarioDeletado.setNumero(-1);
+        usuarioDeletado.setComplemento(null);
+        usuarioDeletado.setBairro("");
+        usuarioDeletado.setCidade("");
+        usuarioDeletado.setUf("--");
+        usuarioDeletado.setCep("00000-000");
+
+        usuarioRepository.save(usuarioDeletado);
     }
 }
