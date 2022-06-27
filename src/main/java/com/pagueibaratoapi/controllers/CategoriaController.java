eae3aae335e3a55d6ae2bab4c08aa01be8dea3a7
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2022-06-26 22:21:40 -0300

    Refatorando

--- src/main/java/com/pagueibaratoapi/controllers/CategoriaController.java
+++ src/main/java/com/pagueibaratoapi/controllers/CategoriaController.java
@@ -2,8 +2,12 @@ package com.pagueibaratoapi.controllers;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,13 +25,42 @@ public class CategoriaController {
         this.categoriaRepository = categoriaRepository;
     }
 
-    @GetMapping(produces = "application/json")
+    @PostMapping
+    public Categoria criar(@RequestBody Categoria requestCategoria) {
+        return categoriaRepository.save(requestCategoria);
+    }
+
+    @GetMapping("/{id}")
+    public Categoria ler(@PathVariable(value = "id") Integer id){
+        return categoriaRepository.findById(id).get();
+    }
+
+    @GetMapping
     public List<Categoria> listar() {
         return categoriaRepository.findAll();
     }
 
-    @PostMapping(consumes = "application/json", produces = "application/json")
-    public Categoria criar(@RequestBody Categoria categoria) {
-        return categoriaRepository.save(categoria);
+    @PatchMapping("/{id}")
+    public Categoria editar(@PathVariable int id, @RequestBody Categoria requestCategoria) {
+        Categoria categoriaAtual = categoriaRepository.findById(id).get();
+        
+        if(requestCategoria.getNome() != null)
+            categoriaAtual.setNome(requestCategoria.getNome());
+
+        if(requestCategoria.getDescricao() != null)
+            categoriaAtual.setDescricao(requestCategoria.getDescricao());
+
+        return categoriaRepository.save(categoriaAtual);
+    }
+
+    @PutMapping("/{id}")
+    public void atualizar(@PathVariable int id, @RequestBody Categoria requestCategoria){
+        requestCategoria.setId(id);
+        categoriaRepository.save(requestCategoria);
+    }
+
+    @DeleteMapping("/{id}")
+    public void remover(@PathVariable int id){
+        categoriaRepository.deleteById(id);
     }
 }
