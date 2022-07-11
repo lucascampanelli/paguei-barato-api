a11101d592a093d2e3e4f524d40ea39016e8d2f5
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2022-07-10 23:11:37 -0300

    Comentando código do controlador da categoria.

--- src/main/java/com/pagueibaratoapi/controllers/CategoriaController.java
+++ src/main/java/com/pagueibaratoapi/controllers/CategoriaController.java
@@ -27,27 +27,42 @@ import com.pagueibaratoapi.repository.CategoriaRepository;
 import com.pagueibaratoapi.utils.EditaRecurso;
 import com.pagueibaratoapi.utils.Tratamento;
 
+/**
+ * Classe responsável por controlar as requisições da categoria.
+ */
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaController {
 
+    // Respository da categoria, responsável pelos comandos JPA no banco.
     private final CategoriaRepository categoriaRepository;
 
+    // Construtor
     public CategoriaController(CategoriaRepository categoriaRepository) {
         this.categoriaRepository = categoriaRepository;
     }
 
+    /**
+     * Rota responsável por criar uma categoria.
+     * @param categoria - Dados da categoria a ser criada.
+     * @return Dados e id da categoria criada.
+     */
     @PostMapping
     public ResponseCategoria criar(@RequestBody Categoria requestCategoria) {
         try {
 
+            // Valida os dados fornecidos.
             Tratamento.validarCategoria(requestCategoria, false);
 
+            // Se existir alguma categoria com o mesmo nome no banco,
             if(categoriaRepository.existsByNomeIgnoreCase(requestCategoria.getNome()))
+                // Retorna erro.
                 throw new DadosConflitantesException("nome_existente");
 
+            // Insere a categoria e transforma os dados obtidos em modelo de resposta.
             ResponseCategoria responseCategoria = new ResponseCategoria(categoriaRepository.save(requestCategoria));
 
+            // Adiciona o link para a categoria.
             responseCategoria.add(
                 linkTo(
                     methodOn(CategoriaController.class).ler(responseCategoria.getId())
@@ -55,6 +70,7 @@ public class CategoriaController {
                 .withSelfRel()
             );
 
+            // Retorna os dados da categoria.
             return responseCategoria;
 
         } catch (DadosConflitantesException e) {
@@ -70,12 +86,19 @@ public class CategoriaController {
         }
     }
 
+    /**
+     * Rota responsável por ler uma categoria específica.
+     * @param id - Id da categoria a ser lida.
+     * @return Dados da categoria.
+     */
     @GetMapping("/{id}")
     public ResponseCategoria ler(@PathVariable("id") Integer id) {
         try {
 
+            // Busca a categoria no banco e transforma os dados obtidos em modelo de resposta.
             ResponseCategoria responseCategoria = new ResponseCategoria(categoriaRepository.findById(id).get());
 
+            // Adiciona o link para a rota de listagem das categorias.
             if(responseCategoria != null) {
                 responseCategoria.add(
                     linkTo(
@@ -85,6 +108,7 @@ public class CategoriaController {
                 );
             }
 
+            // Retorna os dados da categoria.
             return responseCategoria;
 
         } catch (NoSuchElementException e) {
@@ -94,21 +118,30 @@ public class CategoriaController {
         }
     }
 
+    /**
+     * Rota responsável por listar todas as categorias.
+     * @return Lista de categorias.
+     */
     @GetMapping
     public List<ResponseCategoria> listar() {
         try {
 
+            // Busca todas as categorias e cria uma array de resposta.
             List<Categoria> categorias = categoriaRepository.findAll();
             List<ResponseCategoria> responseCategoria = new ArrayList<ResponseCategoria>();
 
+            // Para cada categoria, transforma os dados obtidos em modelo de resposta.
             for(Categoria categoria : categorias) {
                 responseCategoria.add(
                     new ResponseCategoria(categoria)
                 );
             }
 
+            // Se a lista de resposta não estiver vazia,
             if(!responseCategoria.isEmpty()) {
+                // Itera sobres as categorias
                 for(ResponseCategoria categoria : responseCategoria) {
+                    // E adiciona o link para a categoria atual
                     categoria.add(
                         linkTo(
                             methodOn(CategoriaController.class).ler(categoria.getId())
@@ -118,6 +151,7 @@ public class CategoriaController {
                 }
             }
 
+            // Retorna a lista de categorias.
             return responseCategoria;
 
         } catch (NullPointerException e) {
@@ -129,17 +163,30 @@ public class CategoriaController {
         }
     }
 
+    /**
+     * Rota responsável por editar uma categoria.
+     * @param id - Id da categoria a ser editada.
+     * @param categoria - Dados modificados da categoria.
+     * @return Dados novos da categoria editada.
+     */
     @PatchMapping("/{id}")
     public ResponseCategoria editar(@PathVariable int id, @RequestBody Categoria requestCategoria) {
         try {
 
+            // Valida os dados fornecidos.
             Tratamento.validarCategoria(requestCategoria, true);
 
+            // Se existir alguma categoria com o mesmo nome no banco,
             if(categoriaRepository.existsByNomeIgnoreCase(requestCategoria.getNome()))
+                // Retorna erro.
                 throw new DadosConflitantesException("nome_existente");
 
+            // Busca a categoria para ser editada.
             Categoria categoriaAtual = categoriaRepository.findById(id).get();
 
+            // Chama o recurso de tratamento de edição de categoria.
+            // Insere os dados tratados no banco de dados.
+            // Transforma os dados inseridos em resposta.
             ResponseCategoria responseCategoria = new ResponseCategoria(
                 categoriaRepository.save(
                     EditaRecurso.editarCategoria(
@@ -149,6 +196,7 @@ public class CategoriaController {
                 )
             );
 
+            // Adiciona o link para a categoria editada.
             responseCategoria.add(
                 linkTo(
                     methodOn(CategoriaController.class).ler(responseCategoria.getId())
@@ -156,6 +204,7 @@ public class CategoriaController {
                 .withSelfRel()
             );
 
+            // Retorna os dados da categoria editada.
             return responseCategoria;
 
         } catch (DadosConflitantesException e) {
@@ -173,22 +222,36 @@ public class CategoriaController {
         }
     }
 
+    /**
+     * Rota responsável por substituir uma categoria.
+     * @param id - Id da categoria a ser substituída.
+     * @param requestCategoria - Dados da nova categoria.
+     * @return Dados da nova categoria inserida.
+     */
     @PutMapping("/{id}")
     public ResponseCategoria atualizar(@PathVariable int id, @RequestBody Categoria requestCategoria) {
         try {
 
+            // Se existir alguma categoria com o mesmo nome no banco,
             if(categoriaRepository.existsByNomeIgnoreCase(requestCategoria.getNome()))
+                // Retorna erro.
                 throw new DadosConflitantesException("nome_existente");
 
+            // Valida os dados fornecidos.
             Tratamento.validarCategoria(requestCategoria, false);
 
+            // Se não existir alguma categoria com o id fornecido,
             if(!categoriaRepository.existsById(id))
+                // Retorna erro.
                 throw new NoSuchElementException("nao_encontrado");
 
+            // Define o id da categoria como o id fornecido.
             requestCategoria.setId(id);
 
+            // Insere a categoria e transforma os dados inseridos em resposta.
             ResponseCategoria responseCategoria = new ResponseCategoria(categoriaRepository.save(requestCategoria));
 
+            // Adiciona o link para a categoria inserida.
             responseCategoria.add(
                 linkTo(
                     methodOn(CategoriaController.class).ler(responseCategoria.getId())
@@ -196,6 +259,7 @@ public class CategoriaController {
                 .withSelfRel()
             );
 
+            // Retorna os dados da categoria nova.
             return responseCategoria;
 
         } catch (DadosConflitantesException e) {
@@ -213,15 +277,23 @@ public class CategoriaController {
         }
     }
 
+    /**
+     * Rota responsável por excluir uma categoria.
+     * @param id - Id da categoria a ser excluída.
+     */
     @DeleteMapping("/{id}")
     public Object remover(@PathVariable int id) {
         try {
 
+            // Se não existir alguma categoria com o id fornecido,
             if(!categoriaRepository.existsById(id))
+                // Retorna erro.
                 throw new NoSuchElementException("nao_encontrado");
 
+            // Exclui a categoria com o id fornecido.
             categoriaRepository.deleteById(id);
 
+            // Retorna o link para a rota responsável por listar todas as categorias.
             return linkTo(
                         methodOn(CategoriaController.class).listar()
                     )
