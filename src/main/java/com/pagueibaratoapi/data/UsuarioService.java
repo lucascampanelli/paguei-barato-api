c0b5d7ed77b162c42f10260396392f3311d2b57d
Nicholas Campanelli <nicholasoucampanelli@hotmail.com>
2022-07-11 23:56:50 -0300

    Comentário do serviço de usuário.

--- src/main/java/com/pagueibaratoapi/data/UsuarioService.java
+++ src/main/java/com/pagueibaratoapi/data/UsuarioService.java
@@ -9,48 +9,59 @@ import org.springframework.stereotype.Component;
 
 import com.pagueibaratoapi.models.requests.Usuario;
 
+/**
+ * Classe de serviços do usuário.
+ */
 @Component
 public class UsuarioService implements UserDetails {
 
+    // Modelo do usuário.
     private final Optional<Usuario> usuario;
 
+    // Construtor.
     public UsuarioService(Optional<Usuario> usuario) {
         this.usuario = usuario;
     }
 
-    // Autorizações do usuário
+    // Obtém níveis de acesso.
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
     }
 
+    // Obtém senha.
     @Override
     public String getPassword() {
         // Retorna um modelo de usuário null ou sua senha
         return usuario.orElse(new Usuario()).getSenha();
     }
 
+    // Obtém usuário.
     @Override
     public String getUsername() {
         // Retorna um modelo de usuário null ou seu email
         return usuario.orElse(new Usuario()).getEmail();
     }
 
+    // Verifica se o usuário está vencido.
     @Override
     public boolean isAccountNonExpired() {
         return true;
     }
 
+    // Verifica se o usuário está bloqueado.
     @Override
     public boolean isAccountNonLocked() {
         return true;
     }
 
+    // Verifica se a senha está expirada.
     @Override
     public boolean isCredentialsNonExpired() {
         return true;
     }
 
+    // Verifica se o usuário está ativo.
     @Override
     public boolean isEnabled() {
         return true;
