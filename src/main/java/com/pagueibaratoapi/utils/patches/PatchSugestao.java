3d7bdb35684acbf8afd783d675c5c2af39944d7a
Lucas Campanelli <lucas.campanelli@outlook.com>
2022-07-16 00:42:14 -0300

    Comentando a classe responsável pelo Patch da Sugestão

--- src/main/java/com/pagueibaratoapi/utils/patches/PatchSugestao.java
+++ src/main/java/com/pagueibaratoapi/utils/patches/PatchSugestao.java
@@ -2,12 +2,25 @@ package com.pagueibaratoapi.utils.patches;
 
 import com.pagueibaratoapi.models.requests.Sugestao;
 
+/**
+ * Classe resposável pelo patch da Sugestão
+ */
 public class PatchSugestao {
 
+    /**
+     * Método responsável por adicionar os atributos atualizados ao estado atual do ramo.
+     * @param sugestaoAtual - Objeto da sugestão no estado atual do banco de dados.
+     * @param sugestaoEditada - Objeto da sugestão com os atributos que serão atualizados.
+     * @return Sugestao - Objeto da sugestão após a inserção dos atributos atualizados.
+     */
     public static Sugestao edita(Sugestao sugestaoAtual, Sugestao sugestaoEditada) {
+        // Se o preço da sugestão não for nulo
         if(sugestaoEditada.getPreco() != null)
-                sugestaoAtual.setPreco(sugestaoEditada.getPreco() * 100);
+            // Atribui o preço ao atributo preço da sugestão atual multiplicando por 100 (para remover as casas decimais e
+            // salvar como inteiro).
+            sugestaoAtual.setPreco(sugestaoEditada.getPreco() * 100);
         
+        // Retorna a sugestão com os atributos atualizados e os demais atributos no estado atual.
         return sugestaoAtual;
     }
 }
