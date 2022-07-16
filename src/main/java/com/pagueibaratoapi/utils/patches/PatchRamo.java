b9812642ea188bd28f4170d780feb4f5b4614c2b
Lucas Campanelli <lucas.campanelli@outlook.com>
2022-07-16 00:36:54 -0300

    Comentando a classe responsável pelo Patch do Ramo

--- src/main/java/com/pagueibaratoapi/utils/patches/PatchRamo.java
+++ src/main/java/com/pagueibaratoapi/utils/patches/PatchRamo.java
@@ -2,15 +2,27 @@ package com.pagueibaratoapi.utils.patches;
 
 import com.pagueibaratoapi.models.requests.Ramo;
 
+/** Classe resposável pelo patch do Ramo */
 public class PatchRamo {
 
+    /**
+     * Método responsável por adicionar os atributos atualizados ao estado atual do ramo.
+     * @param ramoAtual - Objeto do ramo no estado atual do banco de dados.
+     * @param ramoEditado - Objeto do ramo com os atributos que serão atualizados.
+     * @return Ramo - Objeto do ramo após a inserção dos atributos atualizados.
+     */
     public static Ramo edita(Ramo ramoAtual, Ramo ramoEditado) {
+        // Se o atributo nome não for nulo
         if(ramoEditado.getNome() != null)
+            // Atribui o nome ao atributo nome do ramo atual
             ramoAtual.setNome(ramoEditado.getNome());
 
+        // Se o atributo descrição não for nulo
         if(ramoEditado.getDescricao() != null)
+            // Atribui a descrição ao atributo descrição do ramo atual
             ramoAtual.setDescricao(ramoEditado.getDescricao());
         
+        // Retorna o ramo com os atributos atualizados e os demais atributos no estado atual.
         return ramoAtual;
     }
 }
